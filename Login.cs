dc5e333e3d9c4eb730c2dc4944fe8aab8e749774
krisschacha58-boop <krisswu58@gmail.com>
2026-03-03 18:19:59 +0700

    Add files via upload

---
+++ Login.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectASS
+{
+    public partial class Login : Form
+    {
+       
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(DatabaseConfig.ConnectionString))
+            {
+                conn.Open();   
+
+                MessageBox.Show("Connection Opened Successfully!");
+            }
+
+        }
+    }
+}
